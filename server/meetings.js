8bcbd370b9152975ab71899cd7c81606417574d7
Cassie <cassielevett@gmail.com>
2020-04-11 20:23:40 -0500

    unit tests failing, issue with database perhaps

--- server/meetings.js
+++ server/meetings.js
@@ -13,15 +13,17 @@ const { getAllFromDatabase, addToDatabase, deleteAllFromDatabase, createMeeting 
 
 //GET /api/meetings to get an array of all meetings.
 meetingsRouter.get('/', (req, res, next) => {
-
+  res.send(getAllFromDatabase('meetings'))
 })
 
 //POST /api/meetings to create a new meeting and save it to the database.
 meetingsRouter.post('/', (req, res, next) => {
-
+  let newMeeting = addToDatabase('meetings', createMeeting())
+  res.status(201).send(newMeeting)
 })
 
 //ELETE /api/meetings to delete all meetings from the database.
 meetingsRouter.delete('/', (req, res, next) => {
-
+  deleteAllFromDatabase('meeting')
+  res.status(204).send()
 })
--- server/minions.js
+++ server/minions.js
@@ -1,6 +1,6 @@
 const minionsRouter = require('express').Router()
 
-module.exports = minionsRouter;
+module.exports = minionsRouter
 
 //Database
 const { addToDatabase, getAllFromDatabase, getFromDatabaseById, updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db')
@@ -11,27 +11,44 @@ const { addToDatabase, getAllFromDatabase, getFromDatabaseById, updateInstanceIn
 //title: string
 //salary: number
 
+minionsRouter.param('minionId', (req, res, next, id) => {
+  const minion = getFromDatabaseByID('minions', id)
+  if (minion) {
+    req.minion = minion
+  } else {
+    res.status(404).send();
+  }
+})
+
 //GET /api/minions to get an array of all minions
 minionsRouter.get('/', (req, res, next) => {
-  res.send(req.minion)
+  res.send(getAllFromDatabase('minions'))
 })
 
 //POST /api/minions to create a new minion and save it to the database.
 minionsRouter.post('/', (req, res, next) => {
-
+  const newMinion = addToDatabase('minions', req.body)
+  res.status(201).send(newMinion)
 })
 
 //GET /api/minions/:minionId to get a single minion by id.
 minionsRouter.get('/:minionId', (req, res, next) =>{
-
+  res.send(req.minion)
 })
 
 //PUT /api/minions/:minionId to update a single minion by id.
 minionsRouter.put('/:minionId', (req, res, next) => {
-
+  let updatedMinion = updateInstanceInDatabase('minions', req.body)
+  res.send(updatedMinion)
 })
 
 //DELETE /api/minions/:minionId to delete a single minion by id.
 minionsRouter.delete('/:minionId', (req, res, next) => {
-
+  const deleted = deleteFromDatabasebyId('minions', req.params.minionId)
+  if (deleted) {
+    res.status(204)
+  } else {
+    res.status(500)
+  }
+  res.send()
 })
